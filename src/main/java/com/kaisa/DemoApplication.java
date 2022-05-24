1f15037135992759183f670095634b9adb0e1f6a
kaisa <kaisa817@outlook.com>
2022-05-24 19:38:50 +0800

    kaisa

--- src/main/java/com/kaisa/DemoApplication.java
+++ src/main/java/com/kaisa/DemoApplication.java
@@ -3,11 +3,12 @@ package com.kaisa;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootApplication
+@SpringBootApplication()
 @Slf4j
 @ServletComponentScan
 @EnableTransactionManagement
